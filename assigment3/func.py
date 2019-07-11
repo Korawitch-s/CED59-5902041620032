0dae5d0f2a530dffc7aaa7e6239d380bd5d62e1a
Korawitch Sakolpongpairoj <s5902041620032@email.kmutnb.ac.th>
2019-07-11 14:39:24 +0700

    add files for assignment3

---
+++ assigment3/func.py
@@ -0,0 +1,5 @@
+def returun_42():
+	return 42;
+
+def mt_method(a , b):
+	return a*b;
---
+++ assigment3/main.py
@@ -0,0 +1,3 @@
+from func import *
+print("Result of return_42() is" , return42());
+print("result of 5 x 6) is" , mt_method(5,6))
