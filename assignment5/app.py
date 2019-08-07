a5c135b870c3e8194ad8cc5fb73edfe0fdaa7c65
11 korawitch Sakolpongpairoj <s5902041620032@email.kmutnb.ac.th>
2019-08-08 00:04:58 +0700

    add folder assignment6

--- assignment5/app.py
+++ assignment5/app.py
@@ -3,9 +3,9 @@ from flask import Flask ,render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-   return render_template('Remember.html')
+@app.route('/',methods=["post"])
+def Remember():
+   return 'Hello'
 
 
 
