677b89b961dcbf24589eb4a65b7028e8253fcedc
Korawitch-s <s5902041620032@email.kmutnb.ac.th>
2019-08-08 16:39:49 +0700

    add assignment7

--- assignment7/app.py
+++ assignment7/app.py
@@ -1,5 +1,9 @@
 from flask import Flask ,render_template
-from flask import Flask ,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
 
 app = Flask(__name__)
 
